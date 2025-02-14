1a292e249938f4bcb79dd9c62b2d8f492529644c
icingworld <984442022@qq.com>
2025-02-14 14:21:27 +0800

    refactor: small adjustment fot is_iterator

--- stl/ww_type_traits.h
+++ stl/ww_type_traits.h
@@ -10,14 +10,22 @@ namespace wwstl
 template <class... Ts>
 using void_t = void;
 
-template <typename T, typename = void>
-struct is_iterator : std::false_type {};
+template <
+    class T,
+    class = void
+> class is_iterator
+    : std::false_type
+{
+};
 
 // 如果存在 iterator_traits<T>::iterator_category，则是迭代器
-template <typename T>
-struct is_iterator<T, std::void_t<typename std::iterator_traits<T>::iterator_category>> : std::true_type {};
+template <class T>
+class is_iterator<T, void_t<typename std::iterator_traits<T>::iterator_category>>
+    : std::true_type
+{
+};
 
-template <typename T>
+template <class T>
 constexpr bool is_iterator_v = is_iterator<T>::value;
 
 
